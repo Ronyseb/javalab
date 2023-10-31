62951c92ceea493f880d07b50b0ce1c74a9ef9ff
Rony Sebastian Tomson <146075909+callmezebb@users.noreply.github.com>
2023-10-31 16:09:37 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,26 @@
+package firstjavalab;
+import java.util.Scanner;
+public class Prime {
+	public static void main(String[] args) {
+		int fact=0;
+		Scanner sc=new Scanner(System.in);
+				System.out .println("Enter a number :");
+				int number= sc.nextInt();
+				for(int i=1;i<number;i++)
+				{
+					if(number%i==0)
+					{
+						fact++;
+					}
+				}
+   if(fact==1)
+   {
+	   System.out.println("The Number is prime");
+   }
+   else
+   {
+	   System.out.println("The Number is not Prime");
+   }
+}
+}
+
